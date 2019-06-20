4746fc1955c69d59fd32d3608c3d2d3fc828794d
MichaelJahns <jahnsmic001@gmail.com>
2019-06-20 16:03:13 -0700

    Changes background color for overlay

---
+++ public/js/check.js
@@ -0,0 +1,8 @@
+var checkCollision = function (x, y, array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].x === x && array[i].y === y) {
+            return true;
+        }
+    }
+    return false;
+}
